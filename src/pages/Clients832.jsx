25941651bd7093bcc2dd06365e8bb9f8500ae5fe
Fabian Aichem <fabian.admyn@gmail.com>
2026-03-11 08:52:44 +0000

    File changes

--- src/pages/Clients832.jsx
+++ src/pages/Clients832.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
-import { Plus, Eye, Trash2, ChevronDown, ChevronUp } from "lucide-react";
+import { Plus, Eye, Trash2, ChevronDown, ChevronUp, LayoutDashboard } from "lucide-react";
 import { createPageUrl } from "@/utils";
 import ClientDocuments from "../components/clients/ClientDocuments";
 
